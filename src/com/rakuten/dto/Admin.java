63cf16f60f09bf891af1d1ddff47f398e47e668e
rupal <xyz@gmail.com>
2022-10-19 02:09:45 +0530

    First Version Online Book Store 001

---
+++ src/com/rakuten/dto/Admin.java
@@ -0,0 +1,33 @@
+package com.rakuten.dto;
+
+public class Admin {
+private String name;
+private String emailId;
+private String role;
+private String password;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getEmailId() {
+	return emailId;
+}
+public void setEmailId(String emailId) {
+	this.emailId = emailId;
+}
+public String getRole() {
+	return role;
+}
+public void setRole(String role) {
+	this.role = role;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+}
